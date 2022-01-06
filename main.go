451ee5769ff75c1bec8506278c861ed314ed8d13
Nicholas Ulricksen <n.ulricksen@gmail.com>
2022-01-06 13:21:09 -0800

    create web server

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+	"github.com/segmentio/ksuid"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.POST("/compute", func(c *gin.Context) {
+		// get file
+		file, err := c.FormFile("gpfile")
+		if err != nil {
+			c.JSON(http.StatusUnsupportedMediaType, gin.H{
+				"error": "payload must be type 'multipart/form-data'",
+			})
+			return
+		}
+		fileExt := filepath.Ext(file.Filename)
+
+		// save the file
+		fileId := ksuid.New()
+		fileName := fmt.Sprintf("gpfile%s", fileId.String())
+		gpFileName := fileName + fileExt
+		c.SaveUploadedFile(file, gpFileName)
+
+		// create midi file
+		err = exec.Command("./GuitarProToMidi", gpFileName).Run()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "file could not be converted",
+			})
+			return
+		}
+
+		// return midi file
+		midiFileName := fileName + ".mid"
+		c.File(midiFileName)
+
+		// delete old files
+		os.Remove(gpFileName)
+		os.Remove(midiFileName)
+
+		log.Printf("success: %s --> %s\n", gpFileName, midiFileName)
+	})
+
+	server.Run(":8229")
+}
